2f4996eca03dd87cd9a7529665b5a31c42c1acf8
pedro-leote <pedroleote2005@outlook.pt>
2025-10-16 14:59:44 +0100

    JumpscareScript done V2

--- Assets/Scripts/UI/JumpscareScript.cs
+++ Assets/Scripts/UI/JumpscareScript.cs
@@ -10,22 +10,6 @@ public class JumpscareScript : MonoBehaviour
         _jesterController = GameObject.FindObjectOfType<Animator>().GetComponent<Animator>();
         
     }
-    
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            JumpscareSequence();
-        }
-    }
 
     public void JumpscareSequence()
     {
